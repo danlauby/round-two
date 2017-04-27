31473958c61f6d3d4138df0235582ef0308deeeb
Dan Lauby and jay freeman <jay@>
2017-04-27 15:48:17 -0700

    Notes in js for using jQuery in Drupal

---
+++ sites/all/modules/shirts/scripts.js
@@ -0,0 +1,16 @@
+// ----PHP-----
+//drupal_add_js(array'module_name) =>array(jsVAriable => phpVariable)), array('type' =>'setting');
+//drupal_add_js(drupal_getpath('module', 'module_name') . 'scripts.js');
+
+
+
+
+// -----JS//-------
+// (function($) {
+    // Drupal.behaviors.module_name = {
+    // attach: function(context, settings) {
+    //      var variable = settings.module_name.jsVariable;
+    //      ** Code stays here
+    // }
+    // }
+// })(jQuery);
